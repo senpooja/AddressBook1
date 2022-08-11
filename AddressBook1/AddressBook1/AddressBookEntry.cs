dfb7a86a56ecbd1e57e4677665acea8028abb3b5
senpooja <poojasen744055@gmail.com>
2022-08-11 22:34:02 +0530

    Added AddressBook.cs Files

---
+++ AddressBook1/AddressBook1/AddressBookEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookEntry
+    {
+        List<Contact> addressBook = new List<Contact>();
+
+        internal static Contact CreateContact()
+        {
+            Contact addNew = new Contact();
+            Console.Write("Enter Your First Name:");
+            addNew.FirstName = Console.ReadLine();
+            Console.Write("Enter Your Last Name:");
+            addNew.LastName = Console.ReadLine();
+            Console.Write("Enter Your Address");
+            addNew.Address = Console.ReadLine();
+            Console.Write("Enter Your City Name:");
+            addNew.City = Console.ReadLine();
+            Console.Write("Enter Your State Name:");
+            addNew.State = Console.ReadLine();
+            Console.Write("Enter Your Zipcode:");
+            addNew.PostalCode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Your Phone Number:");
+            addNew.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter Your Email:");
+            addNew.Email = Console.ReadLine();
+            Console.WriteLine("===========================================================================");
+            return addNew;
+
+        }
+    }
+}
